054522231f24b0ca1b73dbf78588e5f67c791d04
JustLikeCheese <854675824@qq.com>
2025-06-24 18:09:58 +0800

    feat: 新增 Type 类型表

--- src/cat.lua
+++ src/cat.lua
@@ -9,29 +9,39 @@ end
 
 cat.global = {}
 cat.globalIds = {}
+
+local Type = {
+    UNDEFINED = 0,
+    STRING = 1,
+    LINK = 2,
+    NUMBER = 3,
+    ARRAY = 4,
+    FUNCTION = 5
+}
+
 cat.load = function(code)
     local lines = string.split(code, "\n")
     local state = {}
     -- .str
-    local strings = string.split(lines[1], " ")
+    local strings = string.split(lines[Type.STRING], " ")
     for i = 1, #strings do
         strings[i] = strings[i]:gsub('\\\\', '\\'):gsub('\\n"', '\n'):gsub('\\c', ' ')
     end
     state.str = strings
     -- .lin
-    local links = string.split(lines[2], " ")
+    local links = string.split(lines[Type.LINK], " ")
     for i = 1, #links do
         links[i] = cat.globalIds[links[i]]
     end
     state.lin = links
     -- .num
-    local numbers = string.split(lines[3], " ")
+    local numbers = string.split(lines[Type.NUMBER], " ")
     for i = 1, #numbers do
         numbers[i] = tonumber(numbers[i])
     end
     state.num = numbers
     -- .arr
-    local arrays = string.split(lines[4], " ")
+    local arrays = string.split(lines[Type.ARRAY], " ")
     for i = 1, #arrays do
         arrays[i] = string.split(arrays[i], ",")
         for j = 1, #arrays[i] do
@@ -41,8 +51,8 @@ cat.load = function(code)
     state.arr = arrays
     -- .fun
     local functions = {}
-    for i = 5, #lines do
-        functions[i - 5] = string.split(lines[i], " ")
+    for i = Type.FUNCTION, #lines do
+        functions[i - Type.FUNCTION] = string.split(lines[i], " ")
     end
     state.fun = functions
     return state
