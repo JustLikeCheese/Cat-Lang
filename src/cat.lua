2f0c2729ca4f1f4094c5234fc14703dd3ef76e43
JustLikeCheese <854675824@qq.com>
2025-06-25 07:06:05 +0800

    feat: 实现 Cat State 的基本操作

--- src/cat.lua
+++ src/cat.lua
@@ -22,24 +22,38 @@ local Type = {
 cat.load = function(code)
     local lines = string.split(code, "\n")
     local state = {}
+    local memory = {}
+    local lengths = {}
+    local length = nil
     -- .str
     local strings = string.split(lines[Type.STRING], " ")
     for i = 1, #strings do
         strings[i] = strings[i]:gsub('\\\\', '\\'):gsub('\\n"', '\n'):gsub('\\c', ' ')
+        table.insert(memory, i)
     end
+    length = #strings
+    table.insert(lengths, length)
     state.str = strings
     -- .lin
     local links = string.split(lines[Type.LINK], " ")
     for i = 1, #links do
-        links[i] = cat.globalIds[links[i]]
+        local globalId = cat.globalIds[links[i]]
+        links[i] = globalId
+        table.insert(memory, globalId)
     end
     state.lin = links
+    length = length + #links
+    table.insert(lengths, length)
     -- .num
     local numbers = string.split(lines[Type.NUMBER], " ")
     for i = 1, #numbers do
-        numbers[i] = tonumber(numbers[i])
+        local number = tonumber(numbers[i])
+        numbers[i] = number
+        table.insert(memory, number)
     end
     state.num = numbers
+    length = length + #numbers
+    table.insert(lengths, length)
     -- .arr
     local arrays = string.split(lines[Type.ARRAY], " ")
     for i = 1, #arrays do
@@ -47,13 +61,38 @@ cat.load = function(code)
         for j = 1, #arrays[i] do
             arrays[i][j] = tonumber(arrays[i][j])
         end
+        table.insert(memory, i)
     end
     state.arr = arrays
+    length = length + #arrays
+    table.insert(lengths, length)
     -- .fun
     local functions = {}
     for i = Type.FUNCTION, #lines do
         functions[i - Type.FUNCTION] = string.split(lines[i], " ")
+        table.insert(memory, i)
     end
+    length = length + #functions
+    table.insert(lengths, length)
     state.fun = functions
+    state.mem = memory
+    state.len = lengths
     return state
+end
+
+cat.type = function(state, addr)
+    for i = 1, #state.len do
+        if addr <= state.len[i] then
+            return i
+        end
+    end
+    return Type.UNDEFINED
+end
+
+cat.get = function(state, addr)
+    return state.mem[addr]
+end
+
+cat.set = function(state, addr, value)
+    state.mem[addr] = value
 end
